7f30c446f62daf28ee2d347d20457e34cb5b49a0
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-05-26 01:44:56 +0530

    solution : question one

---
+++ easy-1/script.js
@@ -0,0 +1,54 @@
+// Problem Statement : Write a program to add 5 numbers. The value of numbers are num1=5, num2=13, num3=7, num4=21 and num5=48.
+
+
+// If the inputs are static or fixed //
+    // var calculateButton = document.querySelector('#calculate-button')
+    // var output = document.querySelector('#output-area')
+
+
+    // function calculateFunction () {
+    //     var calculatedOutput = 5 + 13 + 7 + 21 + 48;
+    //     output.innerText = "Result is : " + calculatedOutput;
+    // }
+
+    // calculateButton.addEventListener("click", calculateFunction)
+// If the inputs are static or fixed //
+
+
+// optimised version of above code
+    var calculateButton = document.querySelector('#calculate-button')
+    var output = document.querySelector('#output-area')
+
+    function calculateFunction () {
+        var sum = 0;
+        var calculatedOutput = [5, 13, 7, 21, 48];
+        for (let i=0; i<calculatedOutput.length; i++) {
+            sum += calculatedOutput[i]
+        }
+        output.innerText = "Result is : " + sum;
+    }
+
+    calculateButton.addEventListener("click", calculateFunction)
+    // base logic : (+=	/ x+=y / x=x+y)
+// optimised version of above code
+
+
+// if inputs are user inputs
+//     var calculateButton = document.querySelector('#calculate-button')
+//     var output = document.querySelector('#output-area')
+
+//     var inputOne = document.querySelector('#input-one')
+//     var inputTwo = document.querySelector('#input-two')
+//     var inputThree = document.querySelector('#input-three')
+//     var inputFour = document.querySelector('#input-four')
+//     var inputFive = document.querySelector('#input-five')
+
+//     function calculateFunction () {
+//         var calculatedOutput = Number(inputOne.value) + Number(inputTwo.value) + Number(inputThree.value) + Number(inputFour.value) + Number(inputFive.value);
+//         output.innerText = "Result is : " + calculatedOutput;
+//     }
+
+
+
+//     calculateButton.addEventListener("click", calculateFunction)
+// if inputs are user inputs
