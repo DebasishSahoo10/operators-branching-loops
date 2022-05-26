ff2048fca08c354e3e7dc7c04f1e2e1c3a2c8771
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-05-26 21:20:56 +0530

    solution : question 7

---
+++ intermediate-1/script.js
@@ -0,0 +1,39 @@
+// problem statemnet : Fizzbuzz - Write a program to return an array from 1 to 100. But for every multiple of 3, replace the number with "Fizz", for every multiple of 5, replace the number with "Buzz" and for every multiples of 3 & 5, replace with "FizzBuzz". Your output should look something like this 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17 .....
+
+
+
+var button = document.querySelector('#print-button')
+
+
+let num = 0;
+
+function printArray() {
+    for (let i=0; i<100; i++){
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+
+    for (let i=0; i<100; i++){
+        if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+
+
+
+button.addEventListener("click", printArray)
