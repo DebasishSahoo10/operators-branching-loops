298d10f674cd53a64c5cd224abe93c1cd90535fa
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-05-27 00:18:34 +0530

    solution : question 9

---
+++ intermediate-3/script.js
@@ -0,0 +1,18 @@
+// problem statement = Write a program to take a number input from user and print multiplication table 12 times for that number.
+
+var printButton = document.querySelector('#print-button')
+var numberInput = document.querySelector('#input-number')
+
+
+
+function printTable () {
+    var number = numberInput.value;
+    let n = 0;
+    for (let i=1; i<10; i++) {
+        console.log(`${number} * ${i} = ${number * i}`)
+    }
+}
+
+
+
+printButton.addEventListener("click", printTable)
